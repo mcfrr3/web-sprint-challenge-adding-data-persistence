575cccfb0e7e1c85fe98585fa41e1918ebe15ca3
Mike Ferrete <m.ferrete88@gmail.com>
2022-05-19 15:19:33 -0800

    completed basic configurations & empty router paths

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,13 @@
 // build your `/api/projects` router here
+const express = require('express');
+const Project = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.end()
+});
+
+router.post('/', (req, res) => {
+  res.end()
+});
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,13 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const Task = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.end()
+});
+
+router.post('/', (req, res) => {
+  res.end()
+});
